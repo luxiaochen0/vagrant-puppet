6211808525526b73d49af5cc1225ec74af8eb8f4
luxiaochen <494985971@qq.com>
2019-03-04 23:41:40 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-sudo rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
-sudo yum install puppet -y
+#官方源 太慢了
+#sudo rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
+
+#自建yum源
+sudo sh -c "cat>/etc/yum.repos.d/puppet6.repo"<<EOF
+[puppet6]
+name=Puppet 6 Repository el 7 - $basearch
+baseurl=http://nas.luxiaochen.com:20082/puppet/puppet6/el/7/$basearch
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet6-release
+enabled=1
+gpgcheck=1
+EOF
+#sudo yum install puppet -y
+#sudo yum install puppetserver -y
